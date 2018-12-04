16804700d7f357c35bfb05b8571a6f77574ddf56
SALAMA John OBS/S EUR <wlwj7741@orange.com>
2018-12-04 20:38:20 +0200

    Create Locations.js file

---
+++ js/locations.js
@@ -0,0 +1,55 @@
+var locations = [
+    {
+        title: 'Republic Palace',
+        lat: 30.089065,
+        long: 31.319383
+    },
+
+    {
+        title: 'Hospital Ganzouri',
+        lat: 30.097253,
+        long: 31.310049
+    },
+
+    {
+        title: '6th of October Garden',
+        lat: 30.108714,
+        long: 31.325137
+    },
+
+    {
+        title: 'El Horreya Mall',
+        lat: 30.093698,
+        long: 31.323954
+    },
+
+    {
+        title: 'Baron Empain Palace',
+        lat: 30.087093,
+        long: 31.330221
+    },
+
+    {
+        title: 'Beirut Hotel',
+        lat: 30.093392,
+        long: 31.327956
+    },
+
+    {
+        title: 'Labour Bureau - Heliopolis',
+        lat: 30.098961,
+        long: 31.329179
+    },
+
+    {
+        title: 'El-Nasr Sporting Club',
+        lat: 30.105300,
+        long: 31.328664
+    },
+
+    {
+        title: 'Tseppas',
+        lat: 30.101040,
+        long: 31.332269
+    }
+];
